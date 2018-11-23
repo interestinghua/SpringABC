5702e0e5dc27368ba4e5e8b5f70ffac61779337a
MarsHua <interestinghua@126.com>
2018-11-23 14:53:13 +0800

    commit

--- src/main/java/com/example/config/ContainerConfig.java
+++ src/main/java/com/example/config/ContainerConfig.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
@@ -17,4 +18,10 @@ public class ContainerConfig implements WebServerFactoryCustomizer<ConfigurableS
         factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
     }
 
+//    @Bean
+//    @Override
+//    public void customize(TomcatServletWebServerFactory factory) {
+//        factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500"));
+//        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
+//    }
 }
